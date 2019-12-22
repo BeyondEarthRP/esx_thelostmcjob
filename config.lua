3997da761e3ff330db6d49c87a5e1940858036aa
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-22 22:46:54 +0000

    edited the config

--- config.lua
+++ config.lua
@@ -4,11 +4,11 @@ Config.MarkerType                 = 22
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
 Config.MarkerColor                = { r = 50, g = 50, b = 204 }
 Config.EnablePlayerManagement     = false
-Config.EnableArmoryManagement     = false
+Config.EnableArmoryManagement     = true
 Config.EnableESXIdentity          = true -- only turn this on if you are using esx_identity
 Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
 Config.EnableSocietyOwnedVehicles = false
-Config.EnableLicenses             = false
+Config.EnableLicenses             = true
 Config.MaxInService               = -1
 Config.Locale                     = 'en'
 
